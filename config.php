9c7df9852bb7fb72f483b1ba6b4c213b2a4c798a
Green <andreyakovlev9@yandex.ru>
2021-11-21 08:19:45 +0300

    Создание констант для главных настроек сайта

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+defined('VG_ACCESS') or die('Доступ запрещен');
+
+const SITE_URL= 'http://modexc.ru';
+const PATH = '/';
+
+const HOST = 'localhost';
+const USER = 'root';
+const PASS = '';
+const DB_NAME = 'modexc';
