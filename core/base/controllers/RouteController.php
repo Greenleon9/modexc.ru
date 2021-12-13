00f9a4d1da848c3162115d87383455137349bac9
Green <andreyakovlev9@yandex.ru>
2021-12-13 10:31:18 +0300

    Заполнен констракт. Добавлены методы и свойства для разбора адресной строки. Метод присваивания контроллеров. Не заполнено условие для административной панели. Не создана функция redirect.

--- core/base/controllers/RouteController.php
+++ core/base/controllers/RouteController.php
@@ -4,12 +4,20 @@
 namespace core\base\controllers;
 
 
+use core\base\exceptions\RouteException;
 use core\base\settings\Settings;
 use core\base\settings\ShopSettings;
 
 class RouteController
 {
   static private $_instance;
+
+  protected $routes;
+  protected $controller;
+  protected $inputMethod;
+  protected $outputMethod;
+  protected $parameters;
+
   private function __clone()
   {
   }
@@ -22,12 +30,60 @@ class RouteController
       }
       return self::$_instance= new self;
   }
-
+// Разбираем путь адресной строки
   private function __construct()
   {
-      $s = Settings::get('routes');
-      $s1 = ShopSettings::get('routes');
-      exit();
+      $address_str = $_SERVER['REQUEST_URI'];
+
+      if (strrpos($address_str, '/') === strlen($address_str)-1 &&
+          strrpos($address_str, '/') !==0){
+          $this->redirect(rtrim($address_str, '/'), 301);
+      }
+      $path = substr($_SERVER['PHP_SELF'], 0, strpos($_SERVER['PHP_SELF'], 'index.php'));
+      if ($path===PATH){
+          $this->routes = settings::get('routes');
+
+          if (!$this->routes) throw new RouteException('Сайт находится на техническом 
+          обслуживании');
+
+          if (strrpos($address_str, $this->routes['admin']['alias'] === strlen(PATH))){
+              //административная панель
+          }else{
+              $url = explode('/', substr($address_str, strlen(PATH)));
+              $hrUrl= $this->routes['user']['hrUrl'];
+
+              $this->controller = $this->routes['user']['path'];
+              $route = 'user';
+          }
+          $this->createRoute($route, $url);
+
+
+          exit();
+      }else {
+          try{
+              throw  new \Exception('Некоректная директория сайта');
+          }
+          catch (\Exception $e){
+              exit($e->getMessage());
+          }
+      }
+  }
+  // Установка котроллеров и методов согласно адресу запроса
+  private function createRoute($var, $arr){
+      $route = [];
+      if (!empty($arr[0])){
+          if ($this->routes[$var]['routes'][$arr[0]]){
+              $route = explode('/', $this->routes[$var]['routes'][$arr[0]]);
+              $this->controller .= ucfirst($route.'Controller');
+          }else {
+              $this->controller .= ucfirst($arr[0].'Controller');
+          }
+      }else{
+          $this->controller .= $this->routes['default']['controller'];
+      }
+      $this->inputMethod = $route[1] ? $route[1] : $this->routes['default']['inputMethod'];
+      $this->outputMethod = $route[2] ? $route[2] : $this->routes['default']['outputMethod'];
+
   }
 
 }
